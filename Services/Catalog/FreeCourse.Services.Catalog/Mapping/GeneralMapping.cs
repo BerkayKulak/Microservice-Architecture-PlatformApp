e626217d9a4b979ed1fcc60fc4f0ccce5e840b91
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-12 13:21:40 +0300

    appsetting.json

--- Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -1,6 +1,18 @@
-﻿namespace FreeCourse.Services.Catalog.Mapping
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.Mapping
 {
-    public class GeneralMapping
+    public class GeneralMapping:Profile
     {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
     }
 }
