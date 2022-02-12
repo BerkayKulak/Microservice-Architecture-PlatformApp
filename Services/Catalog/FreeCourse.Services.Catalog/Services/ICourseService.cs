fd01f83e7423d7e74b09b25c329364be121839b3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-12 14:58:09 +0300

    CourseController

--- Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
@@ -1,6 +1,17 @@
-﻿namespace FreeCourse.Services.Catalog.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Catalog.Services
 {
-    public class ICourseService
+    public interface ICourseService
     {
+        Task<Response<List<CourseDto>>> GetAllAsync();
+        Task<Response<CourseDto>> GetByIdAsync(string id);
+        Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userId);
+        Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
+        Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
     }
 }
