1924cea88ed2c51c64d9e9a960ea2c0a5a700f26
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-13 11:42:24 +0300

    PhotoStock Projenin oluşturulması

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace FreeCourse.IdentityServer.Controllers
 {
@@ -44,6 +45,26 @@ namespace FreeCourse.IdentityServer.Controllers
 
             return NoContent();
 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(new {Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City});
         }
     }
 }
