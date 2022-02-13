7559dc2687828c1bb423a4b4c058c81da143d8e6
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-14 00:13:23 +0300

    MediatR/Query/QueryHandler

--- Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Domain.OrderAggregate;
 
 namespace FreeCourse.Services.Order.Application.Mapping
 {
-    internal class CustomMapping
+    public class CustomMapping : Profile
     {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
     }
 }
