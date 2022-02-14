d19c2878e1a7e6fecdc5979d8ab6d6fbdca5bf9f
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-14 19:58:37 +0300

    CatalogService

---
+++ Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Models.Catalog;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class CatalogService:ICatalogService
+    {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<CategoryViewModel>> GetAllCategoryAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CourseViewModel> GetByCourseId(string courseId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteCourseAsync(string courseId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
