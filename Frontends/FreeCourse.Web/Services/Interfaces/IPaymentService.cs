70dfaee0998950c17967afcc9ce305727826d546
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-15 12:46:40 +0300

    IPaymentService/PaymentService

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using FreeCourse.Web.Models.FakePayments;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput);
+    }
+}
