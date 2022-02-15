fc5d95d356430c1c9c0b96b74a8abd19e96e29e9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-15 13:36:58 +0300

    OrderMicroservice test işlemi

--- Frontends/FreeCourse.Web/Extensions/ServicesExtension.cs
+++ Frontends/FreeCourse.Web/Extensions/ServicesExtension.cs
@@ -8,26 +8,26 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace FreeCourse.Web.Extensions
 {
-    public static class ServicesExtension
+    public static class ServiceExtension
     {
-        public static void AddHttpClientServices(this IServiceCollection services,IConfiguration Configuration)
+        public static void AddHttpClientServices(this IServiceCollection services, IConfiguration Configuration)
         {
             var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
-
             services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
-
             services.AddHttpClient<IIdentityService, IdentityService>();
 
             services.AddHttpClient<ICatalogService, CatalogService>(opt =>
-            {
 
+            {
                 opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}");
             }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
-            services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
-            {
 
+            services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
+
+            {
                 opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.PhotoStock.Path}");
             }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+
             services.AddHttpClient<IUserService, UserService>(opt =>
             {
                 opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
@@ -45,15 +45,13 @@ namespace FreeCourse.Web.Extensions
 
             services.AddHttpClient<IPaymentService, PaymentService>(opt =>
             {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.FakePayment.Path}");
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Payment.Path}");
             }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
             services.AddHttpClient<IOrderService, OrderService>(opt =>
             {
                 opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Order.Path}");
             }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
-
-
         }
     }
 }
