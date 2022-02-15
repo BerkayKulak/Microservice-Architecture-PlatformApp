1b9a4db476a1e483c39a985631462e422c03d228
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-15 11:57:52 +0300

    BasketService ApplyDiscount/CancelApplyDiscount

--- Frontends/FreeCourse.Web/Services/BasketService.cs
+++ Frontends/FreeCourse.Web/Services/BasketService.cs
@@ -11,10 +11,11 @@ namespace FreeCourse.Web.Services
     public class BasketService : IBasketService
     {
         private readonly HttpClient _httpClient;
-
-        public BasketService(HttpClient httpClient)
+        private readonly IDiscountService _discountService;
+        public BasketService(HttpClient httpClient, IDiscountService discountService)
         {
             _httpClient = httpClient;
+            _discountService = discountService;
         }
 
         public async Task<bool> SaveOrUpdate(BasketViewModel basketViewModel)
@@ -98,14 +99,40 @@ namespace FreeCourse.Web.Services
 
         }
 
-        public Task<bool> ApplyDiscount(string discountCode)
+        public async Task<bool> ApplyDiscount(string discountCode)
         {
-            throw new System.NotImplementedException();
+            await CancelApplyDiscount();
+
+            var basket = await Get();
+            if (basket == null)
+            {
+                return false;
+            }
+
+            var hasDiscount = await _discountService.GetDiscount(discountCode);
+            if (hasDiscount == null)
+            {
+                return false;
+            }
+
+            basket.ApplyDiscount(hasDiscount.Code, hasDiscount.Rate);
+            await SaveOrUpdate(basket);
+            return true;
         }
 
-        public Task<bool> CancelApplyDiscount()
+        public async Task<bool> CancelApplyDiscount()
         {
-            throw new System.NotImplementedException();
+            var basket = await Get();
+
+            if (basket == null || basket.DiscountCode == null)
+            {
+                return false;
+            }
+
+            basket.CancelDiscount();
+            await SaveOrUpdate(basket);
+            return true;
         }
+
     }
 }
